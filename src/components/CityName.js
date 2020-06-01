31bbf14faff2f61d9852e9b8428ba9fd41e0711a
Kelvin David <kdroomo416@gmail.com>
2020-06-01 04:37:42 +0000

    Fixing the ability to input alphanumeric characters into the input

--- src/components/CityName.js
+++ src/components/CityName.js
@@ -5,7 +5,7 @@ function Zip(props) {
     const [validationError, setValidationError] = useState(null);
 
     const validate = (event) => {
-        const cityNameValid = /^\d{5}$/;
+        const cityNameValid = /^[a-zA-Z]+$/;
         const valid = cityNameValid.test(event.target.value);
         if (!valid) {
             setValidationError('* Input must be at least 1 character and be only upper/lower case alpha characters');
